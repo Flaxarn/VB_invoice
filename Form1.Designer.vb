5816ca4fd65382ef282b32c37b07e2297492d5de
Kim Ståhl <flaxarn85@gmail.com>
2021-11-18 14:24:19 +0200

    lagt in kod från adressregister och modifierat

---
+++ Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmStart
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtFornamn = New System.Windows.Forms.TextBox()
+        Me.txtEfternamn = New System.Windows.Forms.TextBox()
+        Me.txtAdress = New System.Windows.Forms.TextBox()
+        Me.txtOrt = New System.Windows.Forms.TextBox()
+        Me.txtPostnr = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.btnNy = New System.Windows.Forms.Button()
+        Me.btnSpara = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtPost = New System.Windows.Forms.TextBox()
+        Me.btnForegaende = New System.Windows.Forms.Button()
+        Me.btnSista = New System.Windows.Forms.Button()
+        Me.btnNasta = New System.Windows.Forms.Button()
+        Me.btnForsta = New System.Windows.Forms.Button()
+        Me.lvwFakturarader = New System.Windows.Forms.ListView()
+        Me.btnRadera = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Förnamn"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(93, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Efternamn"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 86)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Adress"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Ort"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 160)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 25)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "PostNr"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 197)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(66, 25)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Datum"
+        '
+        'txtFornamn
+        '
+        Me.txtFornamn.Location = New System.Drawing.Point(116, 9)
+        Me.txtFornamn.Name = "txtFornamn"
+        Me.txtFornamn.Size = New System.Drawing.Size(300, 31)
+        Me.txtFornamn.TabIndex = 6
+        '
+        'txtEfternamn
+        '
+        Me.txtEfternamn.Location = New System.Drawing.Point(116, 46)
+        Me.txtEfternamn.Name = "txtEfternamn"
+        Me.txtEfternamn.Size = New System.Drawing.Size(300, 31)
+        Me.txtEfternamn.TabIndex = 8
+        '
+        'txtAdress
+        '
+        Me.txtAdress.Location = New System.Drawing.Point(116, 83)
+        Me.txtAdress.Name = "txtAdress"
+        Me.txtAdress.Size = New System.Drawing.Size(300, 31)
+        Me.txtAdress.TabIndex = 10
+        '
+        'txtOrt
+        '
+        Me.txtOrt.Location = New System.Drawing.Point(116, 120)
+        Me.txtOrt.Name = "txtOrt"
+        Me.txtOrt.Size = New System.Drawing.Size(300, 31)
+        Me.txtOrt.TabIndex = 11
+        '
+        'txtPostnr
+        '
+        Me.txtPostnr.Location = New System.Drawing.Point(116, 157)
+        Me.txtPostnr.Name = "txtPostnr"
+        Me.txtPostnr.Size = New System.Drawing.Size(300, 31)
+        Me.txtPostnr.TabIndex = 12
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(116, 197)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(300, 31)
+        Me.DateTimePicker1.TabIndex = 13
+        '
+        'btnNy
+        '
+        Me.btnNy.Location = New System.Drawing.Point(540, 12)
+        Me.btnNy.Name = "btnNy"
+        Me.btnNy.Size = New System.Drawing.Size(112, 34)
+        Me.btnNy.TabIndex = 14
+        Me.btnNy.Text = "Ny"
+        Me.btnNy.UseVisualStyleBackColor = True
+        '
+        'btnSpara
+        '
+        Me.btnSpara.Location = New System.Drawing.Point(540, 52)
+        Me.btnSpara.Name = "btnSpara"
+        Me.btnSpara.Size = New System.Drawing.Size(112, 34)
+        Me.btnSpara.TabIndex = 15
+        Me.btnSpara.Text = "Spara"
+        Me.btnSpara.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtPost)
+        Me.GroupBox1.Controls.Add(Me.btnForegaende)
+        Me.GroupBox1.Controls.Add(Me.btnSista)
+        Me.GroupBox1.Controls.Add(Me.btnNasta)
+        Me.GroupBox1.Controls.Add(Me.btnForsta)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 631)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(776, 111)
+        Me.GroupBox1.TabIndex = 16
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'txtPost
+        '
+        Me.txtPost.Location = New System.Drawing.Point(303, 45)
+        Me.txtPost.Name = "txtPost"
+        Me.txtPost.Size = New System.Drawing.Size(150, 31)
+        Me.txtPost.TabIndex = 4
+        Me.txtPost.Text = "txtPost"
+        '
+        'btnForegaende
+        '
+        Me.btnForegaende.Location = New System.Drawing.Point(89, 30)
+        Me.btnForegaende.Name = "btnForegaende"
+        Me.btnForegaende.Size = New System.Drawing.Size(60, 60)
+        Me.btnForegaende.TabIndex = 3
+        Me.btnForegaende.Text = "<"
+        Me.btnForegaende.UseVisualStyleBackColor = True
+        '
+        'btnSista
+        '
+        Me.btnSista.Location = New System.Drawing.Point(710, 30)
+        Me.btnSista.Name = "btnSista"
+        Me.btnSista.Size = New System.Drawing.Size(60, 60)
+        Me.btnSista.TabIndex = 2
+        Me.btnSista.Text = ">>"
+        Me.btnSista.UseVisualStyleBackColor = True
+        '
+        'btnNasta
+        '
+        Me.btnNasta.Location = New System.Drawing.Point(644, 30)
+        Me.btnNasta.Name = "btnNasta"
+        Me.btnNasta.Size = New System.Drawing.Size(60, 60)
+        Me.btnNasta.TabIndex = 1
+        Me.btnNasta.Text = ">"
+        Me.btnNasta.UseVisualStyleBackColor = True
+        '
+        'btnForsta
+        '
+        Me.btnForsta.Location = New System.Drawing.Point(23, 30)
+        Me.btnForsta.Name = "btnForsta"
+        Me.btnForsta.Size = New System.Drawing.Size(60, 60)
+        Me.btnForsta.TabIndex = 0
+        Me.btnForsta.Text = "<<"
+        Me.btnForsta.UseVisualStyleBackColor = True
+        '
+        'lvwFakturarader
+        '
+        Me.lvwFakturarader.HideSelection = False
+        Me.lvwFakturarader.Location = New System.Drawing.Point(12, 234)
+        Me.lvwFakturarader.Name = "lvwFakturarader"
+        Me.lvwFakturarader.Size = New System.Drawing.Size(776, 391)
+        Me.lvwFakturarader.TabIndex = 17
+        Me.lvwFakturarader.UseCompatibleStateImageBehavior = False
+        '
+        'btnRadera
+        '
+        Me.btnRadera.Location = New System.Drawing.Point(540, 93)
+        Me.btnRadera.Name = "btnRadera"
+        Me.btnRadera.Size = New System.Drawing.Size(112, 34)
+        Me.btnRadera.TabIndex = 18
+        Me.btnRadera.Text = "Radera"
+        Me.btnRadera.UseVisualStyleBackColor = True
+        '
+        'frmStart
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 754)
+        Me.Controls.Add(Me.btnRadera)
+        Me.Controls.Add(Me.lvwFakturarader)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnSpara)
+        Me.Controls.Add(Me.btnNy)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.txtPostnr)
+        Me.Controls.Add(Me.txtOrt)
+        Me.Controls.Add(Me.txtAdress)
+        Me.Controls.Add(Me.txtEfternamn)
+        Me.Controls.Add(Me.txtFornamn)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "frmStart"
+        Me.Text = "Faktura"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtFornamn As TextBox
+    Friend WithEvents txtEfternamn As TextBox
+    Friend WithEvents txtAdress As TextBox
+    Friend WithEvents txtOrt As TextBox
+    Friend WithEvents txtPostnr As TextBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents btnNy As Button
+    Friend WithEvents btnSpara As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtPost As TextBox
+    Friend WithEvents btnForegaende As Button
+    Friend WithEvents btnSista As Button
+    Friend WithEvents btnNasta As Button
+    Friend WithEvents btnForsta As Button
+    Friend WithEvents lvwFakturarader As ListView
+    Friend WithEvents btnRadera As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,214 @@
+﻿Public Class frmStart
+    ' Globala variabler
+    Dim recordCount As Integer
+    Dim postNr As Integer
+
+    ' Kontakt för databaskopplingar
+    Public con As New OleDb.OleDbConnection            ' Connection
+    Public ds As New DataSet                           ' Tabeller
+    Public da As New OleDb.OleDbDataAdapter            ' Uppdatera tabeller
+
+    Public Sub dbConnect()
+
+        ' Databas variabler
+        Dim dbProvider As String
+        Dim dbSource As String
+        Dim dbName As String
+        Dim dbPath As String
+        Dim sql As String
+
+        ' Kombinera sökväg och databasnamn till en connection string
+        dbProvider = "PROVIDER=Microsoft.ACE.OLEDB.12.0;"
+        dbName = "faktura.accdb"
+        dbPath = "..\..\..\"
+
+        dbSource = "Data Source= " & dbPath & "\" & dbName
+        con.ConnectionString = dbProvider & dbSource
+
+        ' Öppna databasen
+        con.Open()
+
+        ' Välj data att visa
+        sql = "SELECT * from fakturor;"
+
+        ' Koppla och fyll dataadaptern
+        da = New OleDb.OleDbDataAdapter(sql, con)
+        da.MissingSchemaAction = MissingSchemaAction.AddWithKey
+        da.Fill(ds, "Fakturor")
+    End Sub
+
+    Private Sub frmStart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Koppla databas 
+        dbConnect()
+
+        ' Räkna antal poster
+        recordCount = ds.Tables("Fakturor").Rows.Count
+        txtPost.MaxLength = Math.Ceiling(Math.Log10(recordCount)) ' Ändra längd på inmatning av textruta efter hur många poster som finns
+        postNr = 0
+        fyllFormular(postNr)
+    End Sub
+
+    Public Sub fyllFormular(postNr As Integer)
+
+        ' Hantera felaktiga postinmatningar
+        If postNr > recordCount - 1 Then
+            postNr = recordCount - 1
+        ElseIf postNr < 0 Then
+            postNr = 0
+        End If
+
+        ' Sätt globala variablen
+        Me.postNr = postNr
+
+        ' Fyll formulär
+        txtFornamn.Text = ds.Tables("Fakturor").Rows(postNr)("Fornamn")
+        txtEfternamn.Text = ds.Tables("Fakturor").Rows(postNr)("Efternamn")
+        txtAdress.Text = ds.Tables("Fakturor").Rows(postNr)("Adress")
+        txtPostnr.Text = ds.Tables("Fakturor").Rows(postNr)("Postnr")
+        txtOrt.Text = ds.Tables("Fakturor").Rows(postNr)("Ort")
+        DateTimePicker1.Text = ds.Tables("Fakturor").Rows(postNr)("Datum")
+
+        ' Visa aktuellt postNr ( svåra varianten ^^ )
+        txtPost.Text = postNr + 1
+
+        ' Hantera knappar
+        hanteraKnappar(postNr)
+    End Sub
+
+    Private Sub hanteraKnappar(postNr As Integer)
+
+        ' Hantera enable/disable för knappar
+        btnForsta.Enabled = Not (postNr = 0)
+        btnForegaende.Enabled = Not (postNr = 0)
+
+        btnSista.Enabled = Not (postNr = (recordCount - 1))
+        btnNasta.Enabled = Not (postNr = (recordCount - 1))
+
+        If postNr = -1 Then
+            btnRadera.Enabled = False
+        Else
+            btnRadera.Enabled = True
+        End If
+    End Sub
+
+    Private Sub btnForsta_Click(sender As Object, e As EventArgs) Handles btnForsta.Click
+
+        postNr = 0
+        fyllFormular(postNr)
+    End Sub
+
+    Private Sub btnForegaende_Click(sender As Object, e As EventArgs) Handles btnForegaende.Click
+
+        postNr = postNr - 1
+        fyllFormular(postNr)
+    End Sub
+
+    Private Sub btnNasta_Click(sender As Object, e As EventArgs) Handles btnNasta.Click
+
+        postNr = postNr + 1
+        fyllFormular(postNr)
+    End Sub
+
+    Private Sub btnSista_Click(sender As Object, e As EventArgs) Handles btnSista.Click
+
+        postNr = recordCount - 1
+        fyllFormular(postNr)
+    End Sub
+
+    Private Sub txtPost_Leave(sender As Object, e As EventArgs) Handles txtPost.Leave
+
+        Dim input As Integer
+        input = Val(txtPost.Text)
+        postNr = input - 1
+        fyllFormular(postNr)
+    End Sub
+
+    Private Sub txtPost_KeyDown(sender As Object, e As KeyEventArgs) Handles txtPost.KeyDown
+
+        If e.KeyCode = Keys.Enter Then
+            txtPost_Leave(sender, e)
+        End If
+    End Sub
+
+    Private Sub btnSpara_Click(sender As Object, e As EventArgs) Handles btnSpara.Click
+        saveData()
+    End Sub
+
+    Private Sub saveData()
+
+        Dim cb As New OleDb.OleDbCommandBuilder(da)
+        Dim dbRow As DataRow
+
+        ' Kontroller om ny post eller befintlig
+        If postNr = -1 Then
+            dbRow = ds.Tables("Fakturor").NewRow
+            dbRow.Item("Datum") = Now
+        Else
+            dbRow = ds.Tables("Fakturor").Rows(postNr)
+        End If
+
+        ' Tilldela databasraden nya värden
+        dbRow.Item("Fornamn") = txtFornamn.Text
+        dbRow.Item("Efternamn") = txtEfternamn.Text
+        dbRow.Item("Adress") = txtAdress.Text
+        dbRow.Item("Ort") = txtOrt.Text
+        dbRow.Item("Postnr") = txtPostnr.Text
+
+        ' Uppdatera databasadaptern om ny post
+        If postNr = -1 Then
+            ds.Tables("Fakturor").Rows.Add(dbRow)
+            postNr = ds.Tables("Fakturor").Rows.Count - 1
+            recordCount = ds.Tables("Fakturor").Rows.Count
+        End If
+
+        ' Uppdatera databasraden
+        da.Update(ds, "Fakturor")
+
+        ' Visa posten
+        fyllFormular(postNr)
+    End Sub
+
+    Private Sub btnNy_Click(sender As Object, e As EventArgs) Handles btnNy.Click
+
+        ' Töm formulär
+        txtFornamn.Text = ""
+        txtEfternamn.Text = ""
+        txtAdress.Text = ""
+        txtPostnr.Text = ""
+        txtOrt.Text = ""
+        DateTimePicker1.Text = Now
+
+        ' Indikera ny post
+        postNr = -1
+
+        ' Släck alla knappar
+        hanteraKnappar(postNr)
+    End Sub
+
+    Private Sub btnRadera_Click(sender As Object, e As EventArgs) Handles btnRadera.Click
+
+        If MsgBox("Vill du radera posten?", MsgBoxStyle.YesNo + MsgBoxStyle.Exclamation, "Radera FAktura!") = MsgBoxResult.Yes Then
+            raderaPost()
+        End If
+    End Sub
+
+    Private Sub raderaPost()
+
+        Dim cb As New OleDb.OleDbCommandBuilder(da)
+
+        ' Ta bort posten från datasetet och uppdatera dataadaptern
+        ds.Tables("Fakturor").Rows(postNr).Delete()
+        da.Update(ds, "Fakturor")
+
+        recordCount = ds.Tables("Fakturor").Rows.Count
+        If postNr >= recordCount Then
+            postNr = recordCount - 1
+        End If
+
+        ' Visa följande post
+        fyllFormular(postNr)
+
+        MsgBox("Borta!")
+    End Sub
+End Class
